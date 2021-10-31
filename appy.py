f79c1f9a9c9769d01dcad7319ad9b9947d3abe78
nkcondon <schnooze@hotmail.com>
2021-10-31 08:10:56 -0400

    add files

---
+++ appy.py
@@ -0,0 +1,22 @@
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+
+# Set up database engine for flask /access and query database
+
+
+#Add routing information for each of the other routes
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+
+
